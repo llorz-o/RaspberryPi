d5cd7f5d4a30ee09d1e8ddbd52ce8f2f3095a15c
@_@ <1751777388@qq.com>
2023-03-15 13:45:13 +0800

    save

--- shs/update_frp_config.sh
+++ shs/update_frp_config.sh
@@ -1,7 +1,24 @@
+function err_catch() {
+  if [ "$1" = "0" ]; then
+    echo "success"
+  else
+    echo "catch an error"
+    echo "any type will off this terminal"
+    read -r
+    exit
+  fi
+}
+
 frp_dir=/home/richard/work/frp/frp_0.47.0_linux_arm
-
+app_name=frpc
 file_name=frpc.ini
+# frpc 配置文件路径
+frpc_app_path="$frp_dir/$app_name"
+local_frpc_config_path="$HOME/RaspberryPi/frp_config/frpc.ini"
 
-local_frpc_config="$HOME/RaspberryPi/frp_config/frpc.ini"
+$($frpc_app_path) verify -c "$local_frpc_config_path"
+err_catch $?
 
-cat "$local_frpc_config" >>"$frp_dir/$file_name"
+file_content=$(cat "$local_frpc_config_path")
+echo "$file_content" >>"$frp_dir/$file_name"
+err_catch $?
--- test.sh
+++ test.sh
@@ -1,3 +1,17 @@
-dir=$(dirname "$0")
-echo "$dir"
-read
+function err_catch() {
+  if [ "$1" = "0" ]; then
+    echo "success"
+  else
+    echo "catch an error"
+    echo "any type will off this terminal"
+    read -r
+    exit
+  fi
+}
+
+local_frpc_config_path="$HOME/RaspberryPi/frp_config/frpc.ini"
+
+
+$($frpc_app_path) verify -c "$local_frpc_config_path"
+err_catch $?
+echo 1
