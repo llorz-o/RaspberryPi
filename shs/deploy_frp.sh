f56abe5444089cbc20856d5fb6e05f63e500aa67
@_@ <1751777388@qq.com>
2023-03-27 20:06:47 +0800

    save

--- shs/deploy_frp.sh
+++ shs/deploy_frp.sh
@@ -4,15 +4,17 @@ project_path=$(shell_path "$0")
 
 echo "当前文件夹为：$project_path"
 
-if [ ! -f "$project_path/frp_0.48.0_linux_arm64.tar.gz" ]; then
+frp_path=$project_path/../frp
+
+if [ ! -f "$frp_path/frp_0.48.0_linux_arm64.tar.gz" ]; then
   frp_download_link="https://github.com/fatedier/frp/releases/download/v0.48.0/frp_0.48.0_linux_arm64.tar.gz"
-  cd "$project_path" && wget "$frp_download_link"
+  cd "$frp_path" && wget "$frp_download_link"
 fi
 
-tar -xf "$project_path/frp_0.48.0_linux_arm64.tar.gz" 2>> "$LOG_FILE"
+tar -xf "$frp_path/frp_0.48.0_linux_arm64.tar.gz" 2>> "$LOG_FILE"
 err_catch $? "tar frp gz file"
 
-mv frp_0.48.0_linux_arm64 frp_client
+cd "$frp_path" && mv frp_0.48.0_linux_arm64 frp_client
 err_catch $? "rename frp dir"
 
 cd frp_client || return
--- shs/update_frp_config.sh
+++ shs/update_frp_config.sh
@@ -2,19 +2,20 @@ source "$(cd ~ || return && pwd)/.utils.sh"
 
 project_path=$(shell_path "$0")
 
-frp_dir=$project_path/frp_client
+frp_dir=$project_path/../frp
+frp_client=$frp_dir/frp_client
 app_name=frpc
 file_name=frpc.ini
 # frpc 配置文件路径
-frpc_app_path=$frp_dir/$app_name
-new_config=$project_path/frpc.ini
+frpc_app_path=$frp_client/$app_name
+new_config=$frp_dir/frpc.ini
 
 $frpc_app_path verify -c "$new_config"
 err_catch $? "frpc config verify"
 
 file_content=$(cat "$new_config")
 # > 覆盖写入, >> 追加写入文件
-echo "$file_content" > "$frp_dir/$file_name"
+echo "$file_content" > "$frp_client/$file_name"
 err_catch $? "rewrite config"
 
 pm2 restart server_frp 2>> "$LOG_FILE"
