be7ffbc9c67391ee63fbd29fcfdb01a1853bcb38
@_@ <1751777388@qq.com>
2023-03-27 19:34:42 +0800

    save

--- shs/deploy_frp.sh
+++ shs/deploy_frp.sh
@@ -5,7 +5,7 @@ project_path=$(shell_path "$0")
 echo "当前文件夹为：$project_path"
 
 frp_download_link="https://github.com/fatedier/frp/releases/download/v0.48.0/frp_0.48.0_linux_arm64.tar.gz"
-wget "$frp_download_link"
+cd "$project_path" && wget "$frp_download_link"
 
 tar -xf "frp_0.48.0_linux_arm64.tar.gz" 2 >> "$LOG_FILE"
 err_catch $? "tar frp gz file"
