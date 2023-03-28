59adc282abb784188d70812a914f32e4a4f55d9c
@_@ <1751777388@qq.com>
2023-03-28 15:19:05 +0800

    save

--- server_frp.sh
+++ server_frp.sh
@@ -1,4 +1,4 @@
 source "$(cd ~ && pwd)/.utils.sh"
 dir=$(shell_path "$0")
 cd "$dir"/frp/frp_client || return
-./frpc -c ./frpc.ini
+./frpc -c "$dir/frp/frpc.ini"
--- shs/update_frp_config.sh
+++ shs/update_frp_config.sh
@@ -5,18 +5,18 @@ project_path=$(shell_path "$0")
 frp_dir=$project_path/../frp
 frp_client=$frp_dir/frp_client
 app_name=frpc
-file_name=frpc.ini
-# frpc 配置文件路径
+#file_name=frpc.ini
+## frpc 配置文件路径
 frpc_app_path=$frp_client/$app_name
-new_config=$frp_dir/frpc.ini
+#new_config=$frp_dir/frpc.ini
 
-$frpc_app_path verify -c "$new_config"
+$frpc_app_path verify -c "$frp_dir/frpc.ini"
 err_catch $? "frpc config verify"
 
-file_content=$(cat "$new_config")
+#file_content=$(cat "$new_config")
 # > 覆盖写入, >> 追加写入文件
-echo "$file_content" > "$frp_client/$file_name"
-err_catch $? "rewrite config"
+#echo "$file_content" > "$frp_client/$file_name"
+#err_catch $? "rewrite config"
 
 pm2 restart server_frp 2>> "$LOG_FILE"
 err_catch $? "restart pm2 frp"
