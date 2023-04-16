6745f0d2bc812560bab63e02459ab534aecf984a
@_@ <1751777388@qq.com>
2023-04-16 12:29:12 +0800

    save

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
-echo "crontab run it origin is:$1 whoami is:$(whoami) " >> ~/test_crontab.log 2>&1
-type node >> ~/test_crontab.log 2>&1
-type pm2 >> ~/test_crontab.log 2>&1
-pm2 start /home/pi/RaspberryPi/server_frp_ssh.sh >> ~/test_crontab.log 2>&1
+#echo "crontab run it origin is:$1 whoami is:$(whoami) " >> ~/test_crontab.log 2>&1
+#type node >> ~/test_crontab.log 2>&1
+#type pm2 >> ~/test_crontab.log 2>&1
+#pm2 start /home/pi/RaspberryPi/server_frp_ssh.sh >> ~/test_crontab.log 2>&1
 
+PATH="/usr/bin:/bin:/root/.nvm/versions/node/v19.8.1/bin/node"
+pm2 -h
