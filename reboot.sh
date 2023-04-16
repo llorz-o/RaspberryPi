a9d5ea3c2730430ed993a82f3bdaed440b8b8b96
@_@ <1751777388@qq.com>
2023-04-16 13:30:02 +0800

    save

--- reboot.sh
+++ reboot.sh
@@ -1,16 +1,16 @@
 echo "cron reboot script on time:$(date "+%Y/%m/%d %H:%M:%S")" >>~/reboot.log
 sleep 120
 
-PI_DIR=/home/pi/RaspberryPi
+SERVICES_DIR=/home/pi/RaspberryPi/services
 PM2="/root/.nvm/versions/node/v19.8.1/bin/pm2"
 
 function main() {
 
-  cd "$PI_DIR" && $PM2 start frp-ssh
+  cd "$SERVICES_DIR" && $PM2 start frp-ssh
 
-  cd "$PI_DIR" && $PM2 start frp
+  cd "$SERVICES_DIR" && $PM2 start frp
 
-  cd "$PI_DIR" && $PM2 start cron
+  cd "$SERVICES_DIR" && $PM2 start cron
 }
 
 main >>~/reboot.log 2>&1
