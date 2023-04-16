28f81856f9ab6b9f921d42ba0a26348efc59ba34
@_@ <1751777388@qq.com>
2023-04-16 12:44:18 +0800

    save

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 #type pm2 >> ~/test_crontab.log 2>&1
 #pm2 start /home/pi/RaspberryPi/server_frp_ssh.sh >> ~/test_crontab.log 2>&1
 
-PATH="/usr/bin:/bin:/root/.nvm/versions/node/v19.8.1/bin/node"
-pm2 -h >> ~/test_crontab.log 2>&1
+pm2="/root/.nvm/versions/node/v19.8.1/bin/pm2"
+$pm2 -h >> ~/test_crontab.log 2>&1
